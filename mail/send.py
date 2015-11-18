37397bd0da0491b2efaf7220323601927ca65615
Gareth Jones <gareth@garethpaul.com>
2015-11-18 14:00:00 +0100

    adding send api

---
+++ mail/send.py
@@ -0,0 +1,56 @@
+"""Send an email message from the user's account.
+"""
+
+import base64
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import mimetypes
+import os
+import httplib2
+from google.appengine.api import memcache
+from apiclient.discovery import build
+from apiclient import errors
+http = httplib2.Http(memcache)
+service = build("gmail", "v1", http=http)
+import auth
+
+def SendMessage(user_id, message):
+  """Send an email message.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    message: Message to be sent.
+
+  Returns:
+    Sent Message.
+  """
+  try:
+    message = (service.users().messages().send(userId="me", body=message)
+               .execute(http=auth.getAuth(user_id)))
+    print 'Message Id: %s' % message['id']
+    return message
+  except errors.HttpError, error:
+    print 'An error occurred: %s' % error
+
+def CreateMessage(sender, to, subject, message_text):
+  """Create a message for an email.
+
+  Args:
+    sender: Email address of the sender.
+    to: Email address of the receiver.
+    subject: The subject of the email message.
+    message_text: The text of the email message.
+
+  Returns:
+    An object containing a base64 encoded email object.
+  """
+  message = MIMEText(message_text)
+  message['to'] = to
+  message['from'] = sender
+  message['subject'] = subject
+  return {'raw': base64.b64encode(message.as_string())}
